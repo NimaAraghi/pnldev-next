c35f6b7febec3777f0462bf89d2b59a3c5b0882a
Nima <nims.844@gmail.com>
2025-03-17 14:15:22 +0330

    hello there

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Box from "@/components/Box";
 import Hero from "@/components/Hero";
 import Line from "@/components/Line";
 
@@ -6,6 +7,32 @@ export default function Home() {
     <>
       <Hero />
       <Line title='"آموزش‌ها"' />
+      <div className='grid grid-cols-1 lg:grid-cols-2 mx-auto gap-x-11 gap-y-20 justify-center items-center w-4/6'>
+        <Box
+          title='HTML'
+          image='html.png'
+          desc='اولین قدم برای ساخت یک وبسایت، یادگیری زبان html است. مسیر یادگیری html بسیار ساده است ، پس در این مسیر کوتاه با ما همراه باش!'
+          toturial='html'
+        />
+        <Box
+          title='HTML'
+          image='html.png'
+          desc='اولین قدم برای ساخت یک وبسایت، یادگیری زبان html است. مسیر یادگیری html بسیار ساده است ، پس در این مسیر کوتاه با ما همراه باش!'
+          toturial='html'
+        />
+        <Box
+          title='HTML'
+          image='html.png'
+          desc='اولین قدم برای ساخت یک وبسایت، یادگیری زبان html است. مسیر یادگیری html بسیار ساده است ، پس در این مسیر کوتاه با ما همراه باش!'
+          toturial='html'
+        />
+        <Box
+          title='HTML'
+          image='html.png'
+          desc='اولین قدم برای ساخت یک وبسایت، یادگیری زبان html است. مسیر یادگیری html بسیار ساده است ، پس در این مسیر کوتاه با ما همراه باش!'
+          toturial='html'
+        />
+      </div>
     </>
   );
 }
--- components/Box.tsx
+++ components/Box.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
 import React from "react";
+import Line from "./Line";
+import Link from "next/link";
 
-const Box = () => {
+const Box = ({
+  image,
+  title,
+  desc,
+  toturial,
+  refrence,
+}: {
+  image: string;
+  title: string;
+  desc: string;
+  toturial: string;
+  refrence?: string;
+}) => {
   return (
-    <div>
-      <div></div>
+    <div className='relative flex flex-col w-full p-[1%] bg-green-light-secondry'>
+      <div className='absolute -top-5 right-[5%]'>
+        <img
+          className='h-16'
+          src={`/images/${image}`}
+          alt={`${title} language logo`}
+        />
+      </div>
+      <div className='flex self-end'>
+        <h2>{title}</h2>
+        <Line />
+      </div>
+      <div>
+        <p>{desc}</p>
+      </div>
+      <div className='absolute left-4 -bottom-3'>
+        <Link href={`/toturial/${toturial}`}>آموزش</Link>
+        {refrence && <Link href={`/refrence/${refrence}`}>مرجع</Link>}
+      </div>
     </div>
   );
 };
